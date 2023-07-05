ac970f10a980b72e07d9dd682675bc3e0cca78a1
Noah <noahtochukwu10@gmail.com>
2023-07-05 18:50:14 +0100

    Update models

--- events/models.py
+++ events/models.py
@@ -2,16 +2,31 @@ from django.db import models
 from django.contrib.auth.models import User
 from auth_app.models import User
 import uuid
+from django.utils import timezone
+
 class Venue(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     address = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 class Event(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     description = models.TextField()
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
-    date = models.DateField()
+    date = models.DateTimeField()
     attendees = models.ManyToManyField(User, related_name='events_attending')
+
+    def __str__(self):
+        return self.title
+
+    def is_past_event(self):
+        return self.date < timezone.now().date()
+
+    def get_attendees_count(self):
+        return self.attendees.count()
+
 
--- events/viewsets.py
+++ events/viewsets.py
@@ -1,12 +1,17 @@
 from serializers.events import EventSerializer
 from rest_framework import viewsets
 from .models import Event
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.shortcuts import redirect
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    # authentication_classes = JWTAuthentication
+    permission_classes = [IsAuthenticated]
+    
+    # def dispatch(self, request, *args, **kwargs):
+    #     if not request.user.is_authenticated:
+    #         return redirect('/auth/api/token/')  
+    #     return super().dispatch(request, *args, **kwargs)
