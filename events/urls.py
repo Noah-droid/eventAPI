e32a91b215e1115986966026edb02a8245ed8eb6
LenoFred <frederickallen234@gmail.com>
2023-07-06 11:29:21 +0100

    fixed bugs

--- events/urls.py
+++ events/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     # path('api/events/', views.EventListCreateView.as_view(), name='event-list'),
     # path('api/events/<int:pk>/', views.EventDetailView.as_view(), name='event-detail'),
     path('api/', include(router.urls)),
-    path('api/events/<uuid:event_id>/attendees/', EventAttendeesViewSet.as_view({'get': 'list'}), name='event-attendees'),
+    path('api/events/<event_id>/attendees/', EventAttendeesViewSet.as_view({'get': 'list'}), name='event-attendees'),
     path('api/events/search/', EventSearchViewSet.as_view({'get': 'list'}), name='event-search'),
     path('api/events/<uuid:event_id>/rsvp/', EventViewSet.as_view({'post': 'rsvp'}), name='event-rsvp'),
     path('api/events/<uuid:event_id>/comments/', EventViewSet.as_view({'post': 'comments'}), name='event-comments'),
